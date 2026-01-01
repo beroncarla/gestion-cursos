87c0db7d5bfe8e2d7f0b82f0ec6b3bf5de4cda69
beroncarla <beroncarlaviedma@gmail.com>
2026-01-01 20:12:58 -0300

    test unitarios - no permite inscribir usuario inexistente

--- src/test/java/gestioncursos/service/InscripcionServiceTest.java
+++ src/test/java/gestioncursos/service/InscripcionServiceTest.java
@@ -26,13 +26,49 @@ public class InscripcionServiceTest {
         );
 
 
-        Usuario usuario = new Usuario("carla","cb@correo.com","password");
+        Usuario usuario = new Usuario("carla", "cb@correo.com", "password");
         usuarioPersistence.save(usuario);
-        Curso curso = new Curso("Java Basico","Descripcion de Java Basico");
+        Curso curso = new Curso("Java Basico", "Descripcion de Java Basico");
         cursoPersistence.save(curso);
-        service.inscribirUsuario(usuario.getId(),curso.getId());
+        service.inscribirUsuario(usuario.getId(), curso.getId());
 
-        assertThrows(IllegalStateException.class, ()-> service.inscribirUsuario(usuario.getId(),curso.getId()));
+        assertThrows(IllegalStateException.class, () -> service.inscribirUsuario(usuario.getId(), curso.getId()));
+
+    }
+    @Test
+    public void noPermiteInscribirUsuarioNoExistente(){
+        InscripcionPersistence inscripcionPersistence = new FakeInscripcionPersistence();
+
+        CursoPersistence cursoPersistence = new FakeCursoPersistence();
+
+        UsuarioPersistence usuarioPersistence = new FakeUsuarioPersistence();
+
+        InscripcionService service = new InscripcionServiceImpl(
+                usuarioPersistence,
+                cursoPersistence,
+                inscripcionPersistence
+        );
+
+        Curso curso = new Curso("Java Basico", "Descripcion de Java Basico");
+        cursoPersistence.save(curso);
+
+        assertThrows(IllegalArgumentException.class, () -> service.inscribirUsuario(999, curso.getId()));
 
     }
 }
+    /*
+    @Test
+    public void noPermiteInscribirUsuarioInactivo() {
+        InscripcionPersistence inscripcionPersistence = new FakeInscripcionPersistence();
+
+        CursoPersistence cursoPersistence = new FakeCursoPersistence();
+
+        UsuarioPersistence usuarioPersistence = new FakeUsuarioPersistence();
+
+        InscripcionService service = new InscripcionServiceImpl(
+                usuarioPersistence,
+                cursoPersistence,
+                inscripcionPersistence
+        );
+    }
+*/
