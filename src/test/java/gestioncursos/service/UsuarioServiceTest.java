13eba2a608269a4b01ee5a89025b9201d9ec7680
beroncarla <beroncarlaviedma@gmail.com>
2026-01-03 16:16:47 -0300

    test: no permitir crear usuario con password debil

--- src/test/java/gestioncursos/service/UsuarioServiceTest.java
+++ src/test/java/gestioncursos/service/UsuarioServiceTest.java
@@ -4,14 +4,22 @@ import gestioncursos.persistence.UsuarioPersistence;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UsuarioServiceTest {
 
     @Test
-    public void crearUsuarioCorrectamente(){
+    public void crearUsuarioCorrectamente() {
         UsuarioPersistence usuarioPersistence = new FakeUsuarioPersistence();
         UsuarioService service = new UsuarioServiceImpl(usuarioPersistence);
         service.registrarUsuario("juan", "juan@gmail.com", "securePass");
         assertEquals(1, usuarioPersistence.findAll().size());
+    }
+
+    @Test
+    public void noPermiteCrearUsuarioConPasswordDebil() {
+    UsuarioPersistence usuarioPersistence = new FakeUsuarioPersistence();
+    UsuarioService service = new UsuarioServiceImpl(usuarioPersistence);
+    assertThrows(IllegalArgumentException.class, () -> service.registrarUsuario("maria", "maria@gmail.com", "123"));
     }
 }
